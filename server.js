5dff7a86bc4bd841186b229be86cb6e80000e905
MKen212 <46496040+MKen212@users.noreply.github.com>
2020-06-02 17:48:12 +0200

    Mongoose and DotEnv installed

--- server.js
+++ server.js
@@ -1,3 +1,8 @@
+// Set-up Environment Variables for credentials and default values
+const Dotenv = require("dotenv");
+const dotenvConfig = Dotenv.config();
+if (dotenvConfig.error) console.log(dotenvConfig.error);
+
 // Set-up Express Server
 const express = require("express");
 const app = express();
@@ -5,6 +10,10 @@ const port  = process.env.PORT || 3000;
 
 // Set-up Express Layouts
 const expressLayouts = require("express-ejs-layouts");
+
+// Set-up Mongoose Connection for MongoDB
+const mongoose = require("mongoose");
+const dbURL = process.env.DBSERVER + process.env.DBNAME;
 
 // Set-up Routes Index
 const indexRouter = require("./routes/index");
@@ -17,13 +26,19 @@ app.set("layout", "layouts/layout");
 // Use Express Layouts
 app.use(expressLayouts);
 
-// Set-up directory used to serve static files
+// Use Express.static to serve static files from public directory
 app.use(express.static("./public"));
 
 // Set-up indexRouter to serve the routes
 app.use("/", indexRouter);
 
-// Start Expreess server
+// Connect to MongoDB
+mongoose.connect(dbURL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", error => console.error(error));
+db.once("open", () => console.log(`Connected to MongoDB using database ${db}...`));
+
+// Start Express server
 app.listen(port, function(){
   console.log(`Express HTTP Web Server is running on port ${port}...`);
 });
