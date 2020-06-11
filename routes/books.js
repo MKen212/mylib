cd9cae5d94c009100a16c98c6e26a8c830283ea5
MKen212 <46496040+MKen212@users.noreply.github.com>
2020-06-11 16:04:11 +0200

    Updates to Books Model, Routes & Views

--- routes/books.js
+++ routes/books.js
@@ -11,7 +11,7 @@ const Author = require("../models/author");
 // Set ImageTypes accepted
 const imageMimeTypes = ["image/jpeg", "image/png", "image/gif"];
 
-// Default Get Route - Shows All Books
+// Show All Books
 router.get("/", async function (req, res) {
   let query = Book.find();
   if (req.query.title != null && req.query.title !== "") {
@@ -34,18 +34,12 @@ router.get("/", async function (req, res) {
   }
 });
 
-// Get New Book Route - Show New Book Form
+// Show New Book Form
 router.get("/new", async function (req, res) {
-  renderNewPage(res, new Book(), false);
+  renderFormPage(res, new Book(), "new", false);
 });
 
-router.get("/new", function (req, res) {
-  res.render("authors/new", {
-    author: new Author()
-  });
-});
-
-// Post New Book Route - Create New Book
+// Create New Book
 router.post("/", async function (req, res) {
   let book = new Book({
     title: req.body.title,
@@ -57,23 +51,89 @@ router.post("/", async function (req, res) {
   saveImage(book, req.body.image);
   try {
     let newBook = await book.save();
-    // res.redirect(`books/${newBook.id}`);
-    res.redirect("books/");
+    res.redirect(`books/${newBook.id}`);
   } catch (err) {
-    renderNewPage(res, book, err);
+    renderFormPage(res, book, "new", err);
   }
 });
 
-// Function to show new Books Page
-async function renderNewPage(res, book, hasError) {
+// Show Book
+router.get("/:id", async function (req, res) {
+  try {
+    let book = await Book.findById(req.params.id).populate("authorId").exec();
+    res.render("books/show", {
+      book: book,
+    });
+  } catch (err) {
+    console.error(err);
+    res.redirect("/");
+  }
+});
+
+// Show Edit Book Form
+router.get("/:id/edit", async function (req, res) {
+  try {
+    let book = await Book.findById(req.params.id);
+    renderFormPage(res, book, "edit", false);
+  } catch (err) {
+    res.redirect("books/");
+  }
+});
+
+// Update Book
+router.put("/:id", async function (req, res) {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    book.title = req.body.title;
+    book.authorId= req.body.authorId;
+    book.publishedDate = new Date(req.body.publishedDate);
+    book.pageCount = req.body.pageCount;
+    book.description = req.body.description;
+    saveImage(book, req.body.image);
+    await book.save();
+    res.redirect(`/books/${book.id}`);
+  } catch (err) {
+    if (book != null) {
+      renderFormPage(res, book, "edit", err);
+    } else {
+      res.redirect("/");
+    }
+  }
+});
+
+// Delete Book
+router.delete("/:id", async function (req, res) {
+  let book;
+  try {
+    book = await Book.findById(req.params.id);
+    await book.remove();
+    res.redirect("/books");
+  } catch (err) {
+    if (book == null) {  // book not found
+      res.redirect("/");
+    } else {
+      res.redirect(`/books/${book.id}`);
+    }
+  }
+});
+
+// Function to show Books Form Page
+async function renderFormPage(res, book, form, hasError) {
   try {
     let authors = await Author.find({});
     let params = {
       authors: authors,
       book: book
     };
-    if (hasError) params.errorMessage = `Error creating Author: ${hasError}`;
-    res.render("books/new", params);
+    if (hasError) {
+      if (form == "edit") {
+        params.errorMessage = `Error Updating Book: ${hasError}`;
+      } else {
+        params.errorMessage = `Error Creating Book: ${hasError}`;
+      }
+    }
+    res.render(`books/${form}`, params);
   } catch (err) {
     res.redirect("/books");
   }
@@ -81,7 +141,7 @@ async function renderNewPage(res, book, hasError) {
 
 // Function to add the Image to the book record
 function saveImage(book, imageEncoded) {
-  if (imageEncoded == null) return;
+  if (imageEncoded == null || imageEncoded == "") return;
   let image = JSON.parse(imageEncoded);
   if (image != null && imageMimeTypes.includes(image.type)) {
     book.imageData = new Buffer.from(image.data, "base64");
